8f5c01a5eafd15338b4d581ab411685b34ffdf3f
Rirush <raccoonrirush@gmail.com>
2018-07-17 11:19:49 +0800

    Add logging dependencies

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ package rirush.rtest.server
 
 import io.vertx.core.Vertx
 import io.vertx.ext.web.Router
+import mu.KotlinLogging
 
 fun main(args: Array<String>) {
     val vertx = Vertx.vertx()
@@ -13,6 +14,9 @@ fun main(args: Array<String>) {
         response.putHeader("Content-Type", "text/plain")
                 .end("SERVER STUB; NOT IMPLEMENTED")
     }
+
+    var logger = KotlinLogging.logger {}
+    logger.info { "Starting server on port 8080" }
 
     server.requestHandler { router.accept(it) }.listen(8080)
 }
