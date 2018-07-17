367f2605f6eec9871fb876e11054ccca921c89a9
Rirush <raccoonrirush@gmail.com>
2018-07-17 10:59:20 +0800

    Add Vert.x-Web stub server

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,18 @@
 package rirush.rtest.server
 
+import io.vertx.core.Vertx
+import io.vertx.ext.web.Router
+
 fun main(args: Array<String>) {
+    val vertx = Vertx.vertx()
+    val server = vertx.createHttpServer()
+    val router = Router.router(vertx)
+
+    router.route().handler { ctx ->
+        val response = ctx.response()
+        response.putHeader("Content-Type", "text/plain")
+                .end("SERVER STUB; NOT IMPLEMENTED")
+    }
+
+    server.requestHandler { router.accept(it) }.listen(8080)
 }
